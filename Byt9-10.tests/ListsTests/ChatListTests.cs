f2a95ba2c5ef702d3ab015d44606ddc5b52ae9df
kasztan-nut <135378750+kasztan-nut@users.noreply.github.com>
2023-12-09 18:12:41 +0100

    Update ChatListTests.cs

--- Byt9-10.tests/ListsTests/ChatListTests.cs
+++ Byt9-10.tests/ListsTests/ChatListTests.cs
@@ -1,9 +1,25 @@
-﻿namespace Byt9_10.tests.ListsTests;
+using Byt9_10.Lists;
+using Byt9_10.Models;
+
+namespace Byt9_10.tests.ListsTests;
 
 /// <summary>
 /// Tests for the ChatList class
 /// </summary>
 public class ChatListTests
 {
+    private ChatList _chatList = new(1, new List<Conversation>());
+    
+    [Test]
+    public void GetChatListIdTest()
+    {
+        Console.WriteLine(_chatList.Id);
+    }
+    
+    [Test]
+    public void GetChatListConversationsTest()
+    {
+        Console.WriteLine(_chatList.Conversations);
+    }
     
 }
