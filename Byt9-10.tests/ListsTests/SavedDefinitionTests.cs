506b52abc44643ddade3f8d7e34b450541459c50
kasztan-nut <135378750+kasztan-nut@users.noreply.github.com>
2023-12-09 18:12:07 +0100

    Update SavedDefinitionTests.cs

--- Byt9-10.tests/ListsTests/SavedDefinitionTests.cs
+++ Byt9-10.tests/ListsTests/SavedDefinitionTests.cs
@@ -1,9 +1,25 @@
-﻿namespace Byt9_10.tests.ListsTests;
+using Byt9_10.Lists;
+using Byt9_10.Models;
+
+namespace Byt9_10.tests.ListsTests;
 
 /// <summary>
 /// Tests for the SavedDefinition class
 /// </summary>
 public class SavedDefinitionTests
 {
+    private SavedDefinitionList _savedDefinitionList = new SavedDefinitionList(1, new List<Definition>());
+    
+    [Test]
+    public void GetSavedDefinitionListIdTest()
+    {
+        Console.WriteLine(_savedDefinitionList.Id);
+    }
+    
+    [Test]
+    public void GetSavedDefinitionListDefinitionsTest()
+    {
+        Console.WriteLine(_savedDefinitionList.Definitions);
+    }
     
 }
