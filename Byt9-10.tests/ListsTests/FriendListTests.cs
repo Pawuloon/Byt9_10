c954dab6873dd1358afd87b07fd70e7312659c27
kasztan-nut <135378750+kasztan-nut@users.noreply.github.com>
2023-12-09 18:11:39 +0100

    Update FriendListTests.cs

--- Byt9-10.tests/ListsTests/FriendListTests.cs
+++ Byt9-10.tests/ListsTests/FriendListTests.cs
@@ -1,4 +1,7 @@
-﻿namespace Byt9_10.tests.ListsTests;
+using Byt9_10.Lists;
+using Byt9_10.Models;
+
+namespace Byt9_10.tests.ListsTests;
 
 
 /// <summary>
@@ -6,5 +9,12 @@
 /// </summary>
 public class FriendListTests
 {
+    private FriendsList _friendsList = new(1);
+    
+    [Test]
+    public void GetFriendListIdTest()
+    {
+        Console.WriteLine(_friendsList.Id);
+    }
     
 }
